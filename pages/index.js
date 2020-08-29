255f32a410bf96208bd2c4b3bdb958307de1067b
bayan-404 <bayan.jubah@gmail.com>
2020-08-30 00:41:51 +0300

    add fucntion getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+
+export async function getServerSideProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
 const HomePage = () => {
   return (
     <h1>welcome!</h1>
   )
 }
+
 
 export default HomePage
