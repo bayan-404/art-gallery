9109c3bebbde2effaae28e2967b7c21df4299c6f
bayan-404 <bayan.jubah@gmail.com>
2020-08-30 23:35:25 +0300

    back to normal

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,3 @@
-
-export async function getServerSideProps(context) {
-  return {
-    props: {}, // will be passed to the page component as props
-  }
-}
-
 const HomePage = () => {
   return (
     <h1>welcome!</h1>
